5816900c365969775faaba4428a35a22bee6df6e
Jim Sweeney <jim.sweeney.ios@gmail.com>
2025-11-24 18:20:23 -0500

    Added xcode

---
+++ Bankey/BankeyTests/BankeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BankeyTests.swift
+//  BankeyTests
+//
+//  Created by James Sweeney on 11/24/25.
+//
+
+import Testing
+@testable import Bankey
+
+struct BankeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
